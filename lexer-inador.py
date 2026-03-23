5b259eccff2c33fbb8f5c8640a4f694e0c06752b
Renato Rojas <roj23813@uvg.edu.gt>
2026-03-22 19:55:04 -0600

    solucion de bugs para ciertas cadenas

--- lexer-inador.py
+++ lexer-inador.py
@@ -3,7 +3,7 @@ from ParserYal import YALexParser
 from LexerGenerator import build_dfa_dict
 from lexer_generator import generate_token_module, generate_lexer
 
-parser = YALexParser().parse("Ejemplo básico YALex.txt")
+parser = YALexParser().parse("minipython.yal")
 dfa_dict = build_dfa_dict(parser.get_all_rules(), parser=parser)
 
 generate_token_module(dfa_dict, "myToken.py")
